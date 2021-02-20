3908c1934ed751b440b9a9a08019a2c4f8978ae0
Ian Bruns <ian@rbruns.com>
2021-02-19 17:05:39 -0700

    pets pages up

--- src/AdoptPage/AdoptPage.js
+++ src/AdoptPage/AdoptPage.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PetSquare from '../PetSquare/PetSquare'
+import ApiService from '../services/ApiService'
+import './AdoptPage.css'
 
-export default function AdoptPage(props) {
-    return (
-        <div className='AdoptPage'>
-            <p>Cat</p>
-            <p>Dog</p>
-        </div>
-    )
+export default class AdoptPage extends Component {
+    state = {
+        cat: {},
+        dog: {},
+        people: [],
+        adoptable: true,
+    }
+
+    adoptButtonClicked() {
+        console.log('click');
+    }
+
+    componentDidMount() {
+        ApiService.getCat()
+            .then(cat => {
+                this.setState({ cat })
+            })
+        ApiService.getDog()
+            .then(dog => {
+                this.setState({ dog })
+            })
+    }
+
+    render() {
+        return (
+            <div className='AdoptPage' >
+                <div className='pets'>
+                    {(this.state.cat &&
+                        <PetSquare adoptButtonClicked={this.adoptButtonClicked}
+                            pet={this.state.cat}
+                            adoptable={this.state.adoptable} />)}
+                    {(this.state.dog &&
+                        <PetSquare adoptButtonClicked={this.adoptButtonClicked}
+                            pet={this.state.dog}
+                            adoptable={this.state.adoptable} />)}
+                </div>
+            </div>
+        )
+    }
 }
