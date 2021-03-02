495135ed8ea460b143d46d7dcbeeaf43e178ca9f
Ian Bruns <ian@rbruns.com>
2021-03-02 13:25:46 -0700

    changed height

--- src/PetSquare/PetSquare.js
+++ src/PetSquare/PetSquare.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PetSquare(props) {
     return (
         <div className='PetSquare'>
-            <img src={props.pet.imageURL} alt='' />
+            <img src={props.pet.imageURL} alt='' height='300px' />
             <p>Name: {`${props.pet.name}`}</p>
             <p>Age: {`${props.pet.age}`}</p>
             <p>Breed: {`${props.pet.breed}`}</p>
