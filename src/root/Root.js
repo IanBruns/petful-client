c2ba97d56258d1362c36593a19ccb3c3c86f226d
Ian Bruns <ian@rbruns.com>
2021-03-02 14:52:07 -0700

    light style root

--- src/root/Root.js
+++ src/root/Root.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import AdoptPage from '../AdoptPage/AdoptPage'
 import LandingPage from '../LandingPage/LandingPage'
+import './root.css'
 
 function Root() {
   return <div>
